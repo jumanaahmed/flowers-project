08678b2f812a9c2c6d68f15cfcfa0796d61e5ec0
jumanaahmed <jumanaahmed481@gmail.com>
2026-03-06 23:11:49 +0200

    first commit

---
+++ bloom.js
@@ -0,0 +1,13 @@
+const emotionCard = document.getElementsByClassName('c1')[0];
+
+emotionCard.addEventListener('click', () => {
+    window.location.href = 'emotion.html';
+});
+
+
+const occasionCard = document.getElementsByClassName('c3')[0];
+
+occasionCard.addEventListener('click', () => {
+    window.location.href = 'occasion.html';
+});
+
